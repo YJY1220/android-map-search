31c510302ac7e3535002ebebdb850c576de8b2b9
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-07-08 13:05:03 +0900

    step1 completed

--- app/src/main/java/campus/tech/kakao/map/MapViewModel.kt
+++ app/src/main/java/campus/tech/kakao/map/MapViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+
 class MapViewModel(application: Application) : AndroidViewModel(application) {
     private val repository = MapAccess(application)
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import java.util.Properties
+
+// Function to get API key from local.properties
+fun getApiKey(key: String): String {
+    val properties = Properties()
+    file("local.properties").inputStream().use { properties.load(it) }
+    return properties.getProperty(key)
+}
+
 plugins {
     id("com.android.application") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
