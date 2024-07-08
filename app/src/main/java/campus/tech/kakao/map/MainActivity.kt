c033b054c6570dda5e3ec0b5cfe19b38faedd9be
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-07-08 20:24:46 +0900

    step2 except map image

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,108 +1,27 @@
 package campus.tech.kakao.map
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import campus.tech.kakao.map.databinding.ActivityMainBinding
-import androidx.lifecycle.ViewModelProvider
-import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import campus.tech.kakao.map.databinding.ActivityMapBinding
+import com.kakao.vectormap.MapView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MapViewModel
-    private lateinit var searchAdapter: SearchAdapter
-    private lateinit var selectedAdapter: SelectedAdapter
+    private lateinit var binding: ActivityMapBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel = ViewModelProvider(this).get(MapViewModel::class.java)
+        val mapView = MapView(this)
+        binding.mapViewContainer.addView(mapView)
 
-        setupRecyclerViews()
-        setupSearchEditText()
-        setupClearTextButton()
-        observeViewModel()
-    }
-
-    private fun setupRecyclerViews() {
-        searchAdapter = SearchAdapter { item ->
-            if (viewModel.selectedItems.value?.contains(item) == true) {
-                Toast.makeText(this, getString(R.string.item_already_selected), Toast.LENGTH_SHORT).show()
-            } else {
-                viewModel.selectItem(item)
-            }
+        binding.searchButton.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
         }
-
-        binding.searchResultsRecyclerView.apply {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = searchAdapter
-        }
-
-        selectedAdapter = SelectedAdapter { item -> viewModel.removeSelectedItem(item) }
-        binding.selectedItemsRecyclerView.apply {
-            layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL, false)
-            adapter = selectedAdapter
-        }
-    }
-
-    private fun setupSearchEditText() {
-        binding.searchEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s.toString().isNotEmpty()) {
-                    binding.clearTextButton.visibility = View.VISIBLE
-                } else {
-                    binding.clearTextButton.visibility = View.GONE
-                }
-                viewModel.searchQuery.value = s.toString()
-            }
-
-            override fun afterTextChanged(s: Editable?) {}
-        })
-    }
-
-    private fun setupClearTextButton() {
-        binding.clearTextButton.setOnClickListener {
-            binding.searchEditText.text.clear()
-        }
-    }
-
-    private fun observeViewModel() {
-        viewModel.searchResults.observe(this, Observer { results ->
-            searchAdapter.submitList(results)
-
-            if (results.isEmpty()) {
-                if (viewModel.searchQuery.value.isNullOrEmpty()) {
-                    binding.noResultsTextView.visibility = View.VISIBLE
-                } else {
-                    binding.noResultsTextView.visibility = View.GONE
-                }
-            } else {
-                binding.noResultsTextView.visibility = View.GONE
-            }
-
-            if (results.isEmpty()) {
-                if (viewModel.searchQuery.value.isNullOrEmpty()) {
-                    binding.searchResultsRecyclerView.visibility = View.GONE
-                } else {
-                    binding.searchResultsRecyclerView.visibility = View.VISIBLE
-                }
-            } else {
-                binding.searchResultsRecyclerView.visibility = View.VISIBLE
-            }
-        })
-
-        viewModel.selectedItems.observe(this, Observer { selectedItems ->
-            selectedAdapter.submitList(selectedItems)
-        })
     }
 }
