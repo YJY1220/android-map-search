3b58356848c7ac85aa6dbf0e64cdf02007586555
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-07-08 15:34:29 +0900

    refactor: step1 completed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,3 @@
-import java.util.Properties
-
-// Function to get API key from local.properties
-fun getApiKey(key: String): String {
-    val properties = Properties()
-    file("local.properties").inputStream().use { properties.load(it) }
-    return properties.getProperty(key)
-}
-
 plugins {
     id("com.android.application") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
